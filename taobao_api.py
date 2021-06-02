3dd669e932ac93c6e1dfea4150f6af7186986c67
lpgary <ppinl@163.com>
2021-06-03 00:25:42 +0800

    默认的

--- taobao_api.py
+++ taobao_api.py
@@ -17,11 +17,13 @@ t为时间戳乘以1000
 appKey为固定值，当前为12574478
 data为所发送参数字典，进行json编码并去除':'，','后的空格后所生成的字符串
 """
+
+
 def get_h5_sign():
     _m_h5_tk = ''
     token = _m_h5_tk.split('_')[0]
     appKey = '12574478'
-    t = int(time.time()*1000)
+    t = int(time.time() * 1000)
     data = {}
     data_json = json.dumps(data)
     data_json = re.sub(r',\s"', ',"', data_json)
@@ -31,6 +33,7 @@ def get_h5_sign():
     m.update(e.encode(encoding='utf-8'))
     h5_sign = m.hexdigest()
     return h5_sign
+
 
 def get_h5_link():
     killtime = '2021-05-11 20:00:00'
@@ -47,11 +50,10 @@ def get_h5_link():
         "timeout": "20000",
         "LoginRequest": "true",
         "H5Request": "true",
-        "api": "mtop.taobao.seattle.activitydetail.get",
+        "api": "mtop.taobao.seattle.activity.apply",
         "callback": "mtopjsonp3",
         "data": '{"source":"activityDetail","activityId":"1021017961819530"}'
-    }#source 值如何确定有待研究，目前有观察到"activityDetail"和"memberPlatform2019
+    }  # source 值如何确定有待研究，目前有观察到"activityDetail"和"memberPlatform2019"
     _paras_urlencode = urllib.parse.urlencode(paras)
     h5_url = 'https://h5api.m.taobao.com/h5/mtop.taobao.seattle.activity.apply/1.0/?' + _paras_urlencode
-    return  h5_url
-
+    return h5_url
